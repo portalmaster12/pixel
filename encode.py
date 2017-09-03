9f7e00430594a747d08eac63e59d71d4470c6cdd
portalmaster12 <classifedethan@gmail.com>
2017-09-03 11:51:29 -0400

    hex

--- encode.py
+++ encode.py
@@ -14,8 +14,6 @@ Author: Ethan Bartlett
 
 #encoding
 
-txt = raw_input("Input your message:\n")
-
 alphabet = [' ','a','b','c',
             'd','e','f','g',
             'h','i','j','k',
@@ -23,6 +21,11 @@ alphabet = [' ','a','b','c',
             'p','q','r','s',
             't','u','v','w',
             'x','y','z']
+hexcode = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+txt = raw_input("Input your message:\n")
+
+
+
 mul= []
 num = []
 numd = []
@@ -32,17 +35,48 @@ for l in txt:
 
 print(num)
 
-#f = num/2
+
 
 for l in (num):
-    if l>16:
-#         if f*2==l:            
-        mul.append(1)
-        numd.append(l/2)
+    if l>15:
+         if (l/2)*2==l:
+             mul.append(1)
+             numd.append(l/2)
+         else:
+             mul.append(2)
+             #print(l)
+             numd.append(l/2)
+                    
+        
         
     else:
         mul.append(0)
         numd.append(l)
+
+numhex=[]
+for h in numd:
+    numhex.append(hexcode[h])
+
+
+print (numhex)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 print (mul)
 print (numd)
+print (h)
+print (numhex)
